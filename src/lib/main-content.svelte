b14d5be55fa9abc88796a2ee044c19073ea4374c
Waleed <waleedthedev@gmail.com>
2022-01-12 21:50:43 +0500

    Clean up and setup according to style guides

---
+++ src/lib/main-content.svelte
@@ -0,0 +1,16 @@
+<main class="flex-1 flex flex-col justify-center">
+  <!-- 
+  Order Summary
+
+  You can now listen to millions of songs, audiobooks, and podcasts on any 
+  device anywhere you like!
+
+  Annual Plan
+  $59.99/year
+
+  Change
+
+  Proceed to Payment
+  Cancel Order
+   -->
+</main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,28 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cstm: {
+          primary: {
+            'pale-blue': 'hsl(225, 100%, 94%)',
+            'bright-blue': 'hsl(245, 75%, 52%)',
+          },
+          neutral: {
+            'very-pale-blue': 'hsl(225, 100%, 98%)',
+            'desaturated-blue': 'hsl(224, 23%, 55%)',
+            'dark-blue': 'hsl(223, 47%, 23%)',
+          },
+        },
+      },
+      fontFamily: {
+        'red-hat-display': ['Red Hat Display', 'sans-serif'],
+      },
+    },
   },
 
   plugins: [],
-};
+}
 
-module.exports = config;
+module.exports = config
